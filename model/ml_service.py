634257c9460668730fe607cd8b45f3302a9fc9d8
Patricia Loto <39300655+PatriLoto@users.noreply.github.com>
2020-12-01 23:51:31 -0300

    se completa settings y ml_service

--- model/ml_service.py
+++ model/ml_service.py
@@ -1,24 +1,30 @@
 # -*- coding: utf-8 -*-
 
+import redis
+import settings
+import json
+import time
+from classifier import SentimentClassifier
 ########################################################################
-# COMPLETAR AQUI: Crear conexion a redis y asignarla a la variable "db".
+# COMPLETO: 
 ########################################################################
-db = None
+#  Se crea conexión a redis y se la asigna a la variable "db"
+db = redis.Redis(port=settings.REDIS_PORT, host=settings.REDIS_HOST, db=settings.REDIS_DB_ID)
 ########################################################################
 
 ########################################################################
-# COMPLETAR AQUI: Instanciar modelo de análisis de sentimientos.
+# COMPLETO: 
 # Use classifier.SentimentClassifier de la libreria
 # spanish_sentiment_analysis ya instalada
 ########################################################################
-model = None
+# Se instancia el  modelo de análisis de sentimientos.
+model = SentimentClassifier()
 ########################################################################
-
 
 def sentiment_from_score(score):
     """
     Esta función recibe como entrada el score de positividad
-    de nuestra sentencia y dependiendo su valor devuelve uno
+    de nuestra sentencia y dependiendo su valor devuelve una
     de las siguientes clases:
         - "Positivo": Cuando el score es mayor a 0.55.
         - "Neutral": Cuando el score se encuentra entre 0.45 y 0.55.
@@ -35,10 +41,17 @@ def sentiment_from_score(score):
         Una de las siguientes etiquetas: "Negativo", "Neutral" o "Positivo".
     """
     ####################################################################
-    # COMPLETAR AQUI
+    # COMPLETO
     ####################################################################
     sentiment = None
-    raise NotImplementedError
+    # se agrega el if 
+    if score < 0.45:
+        sentiment = 'Negativo'
+    elif score < 0.55
+        sentiment = 'Neutral'
+    else:
+        sentiment= 'Positivo'    
+
     ####################################################################
 
     return sentiment
@@ -65,12 +78,15 @@ def predict(text):
     score = None
 
     ####################################################################
-    # COMPLETAR AQUI: Utilice el clasificador instanciado previamente
-    # ("model") para obtener el score de positividad.
-    # Luego utilice la función "sentiment_from_score" de este módulo
-    # para obtener el sentimiento ("sentiment") a partir del score.
+    # COMPLETO:
     ####################################################################
-    raise NotImplementedError
+    # Utilizando el clasificador instanciado previamente
+    # ("model") se obtiene el score de positividad.
+    # Y utilizando la función "sentiment_from_score" de este módulo
+    # se obtiene el sentimiento ("sentiment") a partir del score
+    score = model.predict(text)
+    sentiment = sentiment_from_score(score)
+
     ####################################################################
 
     return sentiment, score
@@ -86,33 +102,43 @@ def classify_process():
     # Iteramos intentando obtener trabajos para procesar
     while True:
         ##################################################################
-        # COMPLETAR AQUI: Obtenga un batch de trabajos encolados, use
-        # lrange de Redis. Almacene los trabajos en la variable "queue".
+        # COMPLETO: 
         ##################################################################
-        queue = None
+        
+        # Obtenemos un batch de trabajos encolados, usando lrange de Redis.
+        # Y se almacenan los trabajos en la variable "queue".
+        queue = db.lrange('service_queue',0,9)
         ##################################################################
 
         # Iteramos por cada trabajo obtenido
         for q in queue:
             ##############################################################
-            # COMPLETAR AQUI:
-            #     - Utilice nuestra función "predict" para procesar la
-            #       sentencia enviada en el trabajo.
-            #     - Cree un diccionario con dos entradas: "prediction" y
-            #       "score" donde almacenara los resultados obtenidos.
-            #     - Utilice la funcion "set" de Redis para enviar la
-            #       respuesta. Recuerde usar como "key" el "job_id".
-            #
+            # COMPLETO #
             ##############################################################
-            raise NotImplementedError
+            # Utilizamos la función "predict" para procesar la
+            # sentencia enviada en el trabajo.
+            q = json.loads(q.decode('utf-8'))
+            job_id = q['id']
+            sentiment, score = predict(q['text'])
+
+            # Creamos un diccionario con dos entradas: "prediction" y
+            # "score" donde almacenaremos los resultados obtenidos.
+            output = {'prediction': sentiment, 'score': score}
+
+            # Utilizamos la funcion "set" de Redis para enviar la respuesta.
+            # usamos como "key" el "job_id".
+            db.set(job_id, json.dumps(output))
             ##############################################################
 
         ##################################################################
-        # COMPLETAR AQUI: Use ltrim de Redis para borrar los trabajos ya
-        # procesados. Luego duerma durante unos milisengundos antes de
-        # pedir por mas trabajos.
+        # COMPLETO: 
         ##################################################################
-        raise NotImplementedError
+        # Usamos ltrim de Redis para borrar los trabajos ya procesados. 
+        # Luego dormimos el proceso durante unos milisengundos antes de
+        # pedir por mas trabajos.    
+
+        db.ltrim('service_queue', len(queue), -1)
+        time.sleep(2)
         ##################################################################
 
 
