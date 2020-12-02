6576f2cb3b6ce8b5235e2d36a3821a7bc82b61c0
Patricia Loto <39300655+PatriLoto@users.noreply.github.com>
2020-12-02 01:03:35 -0300

    se agrega un test de uso del endpoint `index`

--- stress_test/locustfile.py
+++ stress_test/locustfile.py
@@ -5,11 +5,12 @@ class QuickstartUser(HttpUser):
 
     @task(1)
     def index(self):
-        raise NotInplementedError
+        self.client.get('/')
 
     @task(3)
     def predict(self):
-        raise NotInplementedError
+       self.client.post('/predict',params={'text':'Esto es una simple prueba para evaluar el comportamiento de nuestra API'})
 
-    def on_start(self):
-        pass
+	def on_start(self):
+	        pass
+
